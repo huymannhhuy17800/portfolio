4ac133bf7b1bf817cf154a0fbe1355e0aae3865f
NguyenManhHuy <68103157+huymannhhuy17800@users.noreply.github.com>
2024-04-05 10:34:31 +0700

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import styles from './Hero.module.css'
-
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 export const Hero = () => {
-    return ( 
+  return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hello, I'm Huy</h1>
-            <h2 className={styles.role}>React Developer</h2>
-            <h3 className={styles.description}>I'm currently  working at FPT software, my goal is to become
-            full-stack developer. This web was created while I'm learning React </h3>
-        </div>
-            <img/>      
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hello, I'm Huy Nguyen</h1>
+        <h2 className={styles.role}>{"<Chicken Developer/>"}</h2>
+        <h3 className={styles.description}>
+          I'm currently working at FPT software, my goal is to become full-stack
+          developer. This web was created while I'm learning React{" "}
+        </h3>
+        <a className={styles.button}>Getting to know me 😊</a>
+      </div>
+      <img
+        src={getImageUrl("hero/hero.png")}
+        className={styles.heroImg}
+        alt="hero-img"
+      />
     </section>
-    )
+  );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,52 @@
-import React ,{useState} from "react";
-import styles from './NavBar.module.css'
+import React, { useState } from "react";
+import styles from "./NavBar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen,setMenuOpen] = useState(false);
-
-    return <nav className={styles.navbar}>
-            <a className={styles.title} href="/">Portfolio</a>
-            <div className={styles.menu}>
-                <img alt="menu-btn" className = {styles.menuBtn} src={menuOpen ? getImageUrl("nav/close.png") : getImageUrl("nav/menu.png")}
-                    onClick={() => setMenuOpen(!menuOpen)}/>
-                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-                    onClick={() => setMenuOpen(false)}>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#">Skills</a></li>
-                    <li><a href="#experience">Experiences</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          alt="menu-btn"
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/close.png")
+              : getImageUrl("nav/menu.png")
+          }
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <img src={getImageUrl("nav/icon/about.png")} alt="" />
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <img src={getImageUrl("nav/icon/skills.png")} alt="" />
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <img src={getImageUrl("nav/icon/experience.png")} alt="" />
+            <a href="#experience">Experiences</a>
+          </li>
+          <li>
+            <img src={getImageUrl("nav/icon/contact.png")} alt="" />
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <img />
+            <a href="#interest">Interest Zone</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
